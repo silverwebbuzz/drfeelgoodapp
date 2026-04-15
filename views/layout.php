b814f787401d4cb4d6454b9d407ea418cb7c80b9
silverwebbuzz <silverwebbuzz@gmail.com>
2026-04-15 16:48:48 +0530

    changes

--- views/layout.php
+++ views/layout.php
@@ -6,7 +6,7 @@
     <title><?php echo htmlspecialchars($page_title ?? 'Dr. Feelgood'); ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
-    <link href="/css/style.css?v=<?php echo filemtime($_SERVER['DOCUMENT_ROOT'].'/css/style.css'); ?>" rel="stylesheet">
+    <link href="/css/style.css?v=<?php echo @filemtime(dirname(__DIR__).'/css/style.css') ?: time(); ?>" rel="stylesheet">
     <link href="/css/datatable.css" rel="stylesheet">
 </head>
 <body>
