29d21ac9920c1be6bf633360353bc3ac0b2fee7e
Vishnu Prajapati <vishnuprajapati@example.com>
2026-06-19 18:04:50 +0530

    added install button on login

--- views/auth/login.php
+++ views/auth/login.php
@@ -215,5 +215,23 @@
             });
         });
     </script>
+<button id="installBtn" style="display:none;">
+Install App
+</button>
+
+<script>
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault();
+    deferredPrompt = e;
+    document.getElementById('installBtn').style.display = 'block';
+});
+
+document.getElementById('installBtn').addEventListener('click', async () => {
+    deferredPrompt.prompt();
+});
+</script>
+
 </body>
 </html>
